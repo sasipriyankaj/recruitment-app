c84a279446de2dd496110ce7bf5b40f32377f8ae
sasipriyankaj <sasipriyankaj.21it@kongu.edu>
2023-11-01 00:14:19 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,5 +4,5 @@ const HOST = process.env.HOST || '0.0.0.0'; // Set the default host to '0.0.0.0'
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, HOST, () => {
-  console.log(`Running on https://${HOST}:${PORT}`);
+  console.log(`Running on http://${HOST}:${PORT}`);
 });

87d40377d459a6994a5fa1d7d4179b249948ba3f
sasipriyankaj <sasipriyankaj.21it@kongu.edu>
2023-10-31 23:24:14 +0530

    Host index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,6 @@
 const app = require('./app');
 
-const HOST = process.env.HOST || '0.0.0.0';
-const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'http://0.0.0.0:8080';
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(HOST);
+console.log(`Running on ${HOST}`);

7f4e523a16cb4cb19ed447c7385e54cd8348dfa2
sasipriyankaj <sasipriyankaj.21it@kongu.edu>
2023-11-01 00:17:08 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const HOST = process.env.HOST || '0.0.0.0'; // Set the default host to '0.0.0.0'
+const HOST = '0.0.0.0'; // Set the default host to '0.0.0.0'
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, HOST, () => {

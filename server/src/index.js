a0e78d5150048dbbbd4a05fc2443b1bd26b93304
sasipriyankaj <sasipriyankaj.21it@kongu.edu>
2023-11-01 00:27:37 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,3 @@
-const app = require('./app');
-
-const HOST = '0.0.0.0'; // Set the default host to '0.0.0.0'
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
-
-
 const app = express();
 app.use(express.json());
 -app.use(express.urlencoded({extended : false}));
@@ -15,3 +5,5 @@ app.use(
   cors({
     origin :["http://localhost:3000", "https://recruitment-app.onrender.com"],})
   );
+
+

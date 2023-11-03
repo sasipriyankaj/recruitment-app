1c3c001767a8549e34591707b335e7efbd09b5df
sasipriyankaj <sasipriyankaj.21it@kongu.edu>
2023-11-03 10:51:34 +0530

    Update index.js

--- client/src/containers/HomeContainer/CompanyContainer/index.js
+++ client/src/containers/HomeContainer/CompanyContainer/index.js
@@ -11,16 +11,28 @@ import NotFound from '../../../components/NotFound/Lazy';
 
 class CompanyContainer extends Component {
   render() {
+    const backgroundImageUrl =
+      'https://static.vecteezy.com/system/resources/previews/005/367/549/non_2x/illustration-of-resume-paper-with-magnifying-glass-suitable-for-design-element-of-job-vacancy-poster-recruitment-agency-website-background-and-resume-builder-service-vector.jpg';
+
+    const companyStyle = {
+      backgroundImage: `url(${backgroundImageUrl})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundAttachment: 'fixed',
+    };
+
     return (
-      <Routes>
-        <Route index element={<Company />} />
-        <Route path="students" element={<StudentsContainer />} />
-        <Route path="jobs" element={<JobsContainer />} />
-        <Route path="jobs/new" element={<NewContainer />} />
-        <Route path="profile" element={<ProfileContainer />} />
-        <Route path="profile/edit" element={<EditContainer />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div style={companyStyle}>
+        <Routes>
+          <Route index element={<Company />} />
+          <Route path="students" element={<StudentsContainer />} />
+          <Route path="jobs" element={<JobsContainer />} />
+          <Route path="jobs/new" element={<NewContainer />} />
+          <Route path="profile" element={<ProfileContainer />} />
+          <Route path="profile/edit" element={<EditContainer />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     );
   }
 }

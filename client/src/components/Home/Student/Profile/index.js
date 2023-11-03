1e216eabac234b46c090ec96f833b281834eb117
sasipriyankaj <sasipriyankaj.21it@kongu.edu>
2023-11-03 16:16:14 +0530

    Update index.js

--- client/src/components/Home/Student/Profile/index.js
+++ client/src/components/Home/Student/Profile/index.js
@@ -7,7 +7,16 @@ import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 import * as ROUTES from '../../../../constants/routes';
 
-const Profile = ({ firstName, lastName, email, address, phone, coverLetter, gender }) => {
+const Profile = ({
+  firstName,
+  lastName,
+  email,
+  address,
+  phone,
+  coverLetter,
+  gender,
+  resume, // Add resume
+}) => {
   const navigate = useNavigate();
 
   return (
@@ -20,72 +29,41 @@ const Profile = ({ firstName, lastName, email, address, phone, coverLetter, gend
           <Form>
             <Form.Group controlId="firstName" className="mb-3">
               <Form.Label>First Name</Form.Label>
-              <Form.Control
-                readOnly
-                type="text"
-                name="firstName"
-                value={firstName}
-              />
+              <Form.Control readOnly type="text" name="firstName" value={firstName} />
             </Form.Group>
-            <Form.Group controlId="lastName" className="mb-3">
+            <Form.Group controlId="lastName" className "mb-3">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                readOnly
-                type="text"
-                name="lastName"
-                value={lastName}
-              />
+              <Form.Control readOnly type="text" name="lastName" value={lastName} />
             </Form.Group>
-            {/* <Form.Group controlId="email" className="mb-3">
+            <Form.Group controlId="email" className="mb-3">
               <Form.Label>Email</Form.Label>
-              <Form.Control
-                readOnly
-                type="text"
-                name="email"
-                value={email}
-              />
-            </Form.Group> */}
+              <Form.Control readOnly type="text" name="email" value={email} />
+            </Form.Group>
             <Form.Group controlId="address" className="mb-3">
               <Form.Label>Address</Form.Label>
-              <Form.Control
-                readOnly
-                type="text"
-                name="address"
-                value={address}
-              />
+              <Form.Control readOnly type="text" name="address" value={address} />
             </Form.Group>
             <Form.Group controlId="phone" className="mb-3">
               <Form.Label>Phone</Form.Label>
-              <Form.Control
-                readOnly
-                type="text"
-                name="phone"
-                value={phone}
-              />
+              <Form.Control readOnly type="text" name="phone" value={phone} />
             </Form.Group>
             <Form.Group controlId="coverLetter" className="mb-3">
               <Form.Label>Cover Letter</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={4}
-                readOnly
-                name="coverLetter"
-                value={coverLetter}
-              />
+              <Form.Control as="textarea" rows={4} readOnly name="coverLetter" value={coverLetter} />
             </Form.Group>
             <Form.Group controlId="gender" className="mb-3">
               <Form.Label>Gender</Form.Label>
-              <Form.Control
-                readOnly
-                type="text"
-                name="gender"
-                value={gender}
-              />
+              <Form.Control readOnly type="text" name="gender" value={gender} />
             </Form.Group>
-            <Button
-              variant="success"
-              onClick={() => navigate(ROUTES.PROFILE_EDIT)}
-            >
+            {resume && (
+              <div>
+                <Form.Label>Resume</Form.Label>
+                <a href={resume} download>
+                  Download Resume
+                </a>
+              </div>
+            )}
+            <Button variant="success" onClick={() => navigate(ROUTES.PROFILE_EDIT)}>
               Edit
             </Button>
           </Form>
@@ -103,6 +81,7 @@ Profile.propTypes = {
   phone: PropTypes.string.isRequired,
   coverLetter: PropTypes.string.isRequired,
   gender: PropTypes.string.isRequired,
+  resume: PropTypes.string, // Update the prop type for resume
 };
 
 export default Profile;

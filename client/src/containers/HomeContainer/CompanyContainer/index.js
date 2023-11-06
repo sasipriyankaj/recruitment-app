9ef623d9fe5a8c376201e3985a6ed886b90b7f80
sasipriyankaj <sasipriyankaj.21it@kongu.edu>
2023-11-06 17:10:03 +0530

    Update index.js

--- client/src/containers/HomeContainer/CompanyContainer/index.js
+++ client/src/containers/HomeContainer/CompanyContainer/index.js
@@ -10,23 +10,7 @@ import EditContainer from './ProfileContainer/EditContainer/Lazy';
 import NotFound from '../../../components/NotFound/Lazy';
 
 class CompanyContainer extends Component {
-  render() {
-    const backgroundImageUrl =
-      'https://img.freepik.com/free-vector/recruitment-agency-searching-job-applicants_1262-19873.jpg?w=996&t=st=1698990045~exp=1698990645~hmac=8d3ffe62f92f26a83029bdbd9609665c2f4e87a79f925f38316b2fa4804d8706';
-
-    const bodyStyle = {
-      backgroundImage: `url(${backgroundImageUrl})`,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundAttachment: 'fixed',
-      height: '100vh',
-      margin: 0,
-      padding: 0,
-      display: 'flex',
-      flexDirection: 'column',
-    };
-
-    return (
+     return (
       <body style={bodyStyle}>
         <Routes>
           <Route index element={<Company />} />
